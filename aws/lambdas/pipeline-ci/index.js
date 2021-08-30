43b72f55c64c9d749f89dfeafe932404bbd38ac1
ala <ala@zmags.com>
2021-08-30 17:35:31 +0200

    trigger hook

--- aws/lambdas/pipeline-ci/index.js
+++ aws/lambdas/pipeline-ci/index.js
@@ -1,13 +1,49 @@
 const AWS = require("aws-sdk");
+const crypto = require('crypto');
 const codePipeline = new AWS.CodePipeline()
 const { CodePipelineClient, StartPipelineExecutionCommand} = require("@aws-sdk/client-codepipeline");
 
+const region = "eu-west-1";
+const secretName = "github/webhooks/mfe-pipeline";
+let secret;
+let decodedBinarySecret;
+
+
 const client = new CodePipelineClient({
-    region: "eu-west-1"
+    region: region
+});
+
+var clientSecretManager = new AWS.SecretsManager({
+    region: region
 });
 
 exports.handler = async (event) => {
-    const microFrontendNames = findMicroFrontendNames( event["commits"][0]);
+    let secret;
+    try {
+        const response = await clientSecretManager.getSecretValue({SecretId: secretName}).promise();
+        secret = JSON.parse(response.SecretString)["GITHUB_WEBHOOK_SECRET"];
+        console.log("SSSSSS", response.SecretString);
+         console.log("SSSSS22S", secret);
+    } catch (e) {
+        console.log(e);
+        return {
+            statusCode: 401,
+            body: JSON.stringify('Could not get secret.'),
+        };
+    }
+
+
+    const isValid = validateGitHubAuthentication(event, secret);
+
+    if (!isValid) {
+        return {
+            statusCode: 401,
+            body: JSON.stringify('Unauthorized api caller.'),
+        };
+    }
+    const body = JSON.parse(event.body);
+
+    const microFrontendNames = findMicroFrontendNames( body["commits"][0]);
 
     if (!microFrontendNames || !microFrontendNames.length) {
         return {
@@ -38,11 +74,21 @@ exports.handler = async (event) => {
 
 };
 
-function findMicroFrontendNames(latestCommit) {
-    console.log("Try",  latestCommit["modified"]
-        .concat(latestCommit["added"])
-        .concat(latestCommit["removed"]));
 
+
+function validateGitHubAuthentication(event, secret) {
+    const {body, headers} = event;
+    const headerSignature = headers["X-Hub-Signature-256"];
+
+    const decryptedSignature = crypto
+         .createHmac('sha256', secret)
+         .update(JSON.stringify(body))
+         .digest('hex');
+
+    return headerSignature !== decryptedSignature;
+}
+
+function findMicroFrontendNames(latestCommit) {
     const mfeNames = latestCommit["modified"]
         .concat(latestCommit["added"])
         .concat(latestCommit["removed"])
@@ -58,6 +104,9 @@ function findMicroFrontendNames(latestCommit) {
             }
 
             return filePath.split("/")[0];
+        })
+        .filter(name => {
+            return name.startsWith("mfe-");
         });
 
     return [...new Set(mfeNames)];
